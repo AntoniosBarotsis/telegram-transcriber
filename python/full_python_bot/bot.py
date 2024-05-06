19a24a8fd5c69a9bc7c6f953ed8079ddc663d96c
Dragos Balan <balandragos5555@gmail.com>
2024-05-06 19:32:10 +0200

    Avoid terminal log spam (from httpx)

--- python/full_python_bot/bot.py
+++ python/full_python_bot/bot.py
@@ -19,6 +19,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+logging.getLogger('httpx').setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
